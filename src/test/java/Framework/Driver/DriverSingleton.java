7842e06de1400fecb35fbc8c141f3cc7fb29c7a0
Igor Verza <Neort@mail.ru>
2022-01-14 18:48:33 +0300

    update

--- src/test/java/Framework/Driver/DriverSingleton.java
+++ src/test/java/Framework/Driver/DriverSingleton.java
@@ -1,6 +1,7 @@
 package Framework.Driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -24,7 +25,7 @@ public class DriverSingleton {
                     driver = new ChromeDriver();
                 }
             }
-            driver.manage().window().maximize();
+            driver.manage().window().setSize(new Dimension(1920, 1080));
         }
         return driver;
     }
